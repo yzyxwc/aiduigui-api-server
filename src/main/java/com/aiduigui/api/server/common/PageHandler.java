256cefac66ebf26cc8476606475c9c2fe065029c
wuchao <wuc@koal.com>
2022-03-04 16:00:53 +0800

    分页工具

---
+++ src/main/java/com/aiduigui/api/server/common/PageHandler.java
@@ -0,0 +1,27 @@
+package com.aiduigui.api.server.common;
+
+import com.github.pagehelper.PageHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * @author koal_wc
+ * @description 分页插件
+ * @date create at 2022/3/4 15:56
+ */
+@Configuration
+public class PageHandler {
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
+}
